b892c93ae7d90ef6a8d38105f095d0c26ba988ca
sabir546 <mdgulamsabir9@gmail.com>
2024-04-15 03:51:19 +0530

    error handeling done task completed,

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("taxForm").addEventListener("submit", function (event) {
     parseFloat(document.getElementById("extraIncome").value) || 0; // Default to 0 if empty or invalid
   const deductions =
     parseFloat(document.getElementById("deductions").value) || 0; // Default to 0 if empty or invalid
-  
+
   // Validate inputs
   let hasError = false;
   if (!age) {
@@ -55,5 +55,65 @@ document.getElementById("taxForm").addEventListener("submit", function (event) {
       .addEventListener("click", function () {
         modal.style.display = "none";
       });
+  }
+});
+
+//  hover on question marks show the details
+document.querySelector(".annual").addEventListener("mouseenter", function (e) {
+  document.querySelector(".modal2").style.display = "block";
+});
+
+document.querySelector(".annual").addEventListener("mouseleave", function (e) {
+  document.querySelector(".modal2").style.display = "none";
+});
+
+//Shows the error if you enter any non numerical value
+//
+//For INCOME INPUT
+
+const incomeInput = document.getElementById("income");
+const tooltipIcon = document.querySelector(".tooltip");
+const errorMessage = document.getElementById("error-message");
+
+incomeInput.addEventListener("input", function () {
+  const isValid = /^\d+$/.test(this.value);
+  if (!isValid) {
+    errorMessage.style.display = "block";
+    tooltipIcon.style.color = "red"; // Change tooltip color to red
+  } else {
+    errorMessage.style.display = "none";
+    tooltipIcon.style.color = "black"; // Reset tooltip color
+  }
+});
+
+// FOR EXTRA INCOME INPUT
+
+const extraIncome = document.getElementById("extraIncome");
+const tooltipIcon2 = document.querySelector(".tooltip2");
+const errorMessage2 = document.getElementById("error-message2");
+
+extraIncome.addEventListener("input", function () {
+  const isValid = /^\d+$/.test(this.value);
+  if (!isValid) {
+    errorMessage2.style.display = "block";
+    tooltipIcon2.style.color = "red"; // Change tooltip color to red
+  } else {
+    errorMessage2.style.display = "none";
+    tooltipIcon2.style.color = "black"; // Reset tooltip color
+  }
+});
+
+// FOR DEDUCTION INPUT
+const deductions = document.getElementById("deductions");
+const tooltipIcon3 = document.querySelector(".tooltip3");
+const errorMessage3 = document.getElementById("error-message3");
+deductions.addEventListener("input", function () {
+  const isValid = /^\d+$/.test(this.value);
+  if (!isValid) {
+    errorMessage3.style.display = "block";
+    tooltipIcon3.style.color = "red"; // Change tooltip color to red
+  } else {
+    errorMessage3.style.display = "none";
+    tooltipIcon3.style.color = "black"; // Reset tooltip color
   }
 });
